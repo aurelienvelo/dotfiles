636e54cb375f79508247f5e1943ceb27a1d58374
aurelienvelo <aurelien.velo@gmail.com>
2026-07-21 11:37:00 +0200

    zsh history config

--- home/private_dot_config/zsh/history.zsh
+++ home/private_dot_config/zsh/history.zsh
@@ -1,6 +1,23 @@
 # Configuration de base de l'historique
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
-setopt appendhistory
-setopt sharehistory
+
+# Emplacement
+HISTFILE="${XDG_STATE_HOME:-$HOME/.local/state}/zsh/history"
+#mkdir -p "${HISTFILE:h}"
+
+# Taille
+HISTSIZE=100000
+SAVEHIST=100000
+
+# Sauvegarde
+setopt APPEND_HISTORY
+setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
+
+# Nettoyage
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_FIND_NO_DUPS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_REDUCE_BLANKS
+setopt HIST_VERIFY
