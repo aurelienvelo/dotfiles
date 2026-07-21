153ed9f03170ef59fddc52b4c1b83c47b8e15974
aurelienvelo <aurelien.velo@gmail.com>
2026-07-21 16:52:50 +0200

    add ghostty & tmux + plugins

--- home/private_dot_config/zsh/plugins.zsh
+++ home/private_dot_config/zsh/plugins.zsh
@@ -11,3 +11,10 @@ fi
 if [[ -f /usr/share/fzf/completion.zsh ]]; then
     source /usr/share/fzf/completion.zsh
 fi
+
+# Tmux
+if command -v tmux >/dev/null &&
+  [ -z "$TMUX" ] && [ -n "$PS1" ]; then
+    ~/.local/bin/tmux-bootstrap
+    exec tmux
+fi
