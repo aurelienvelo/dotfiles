e925074b3cca28206b4ea5c75c14dd26f02992d2
aurelienvelo <aurelien.velo@gmail.com>
2026-09-16 20:07:41 +0200

    fix: data chezmoi non rendu

--- home/.chezmoitemplates/pkg-list.sh
+++ home/.chezmoitemplates/pkg-list.sh
@@ -1,40 +1,45 @@
 {{ if eq .chezmoi.os "linux" -}}
+{{- $gui := dig "gui" false . -}}
+{{- $workstation := dig "workstation" false . -}}
+{{- $device_type := dig "device_type" "unknown" . -}}
+{{- $has_nvidia := dig "has_nvidia" false . -}}
+{{- $has_battery := dig "has_battery" false . -}}
 desired_packages() {
   local pkgs=(
 {{ range .packages.arch.pacman }}{{ "  " }}{{ . | quote }}
 {{ end }}{{ range .packages.arch.aur }}{{ "  " }}{{ . | quote }}
 {{ end }}  )
-{{ if ne .device_type "server" }}
+{{ if ne $device_type "server" }}
   local netbird_pkgs=(
 {{ range .packages.netbird.aur }}{{ "  " }}{{ . | quote }}
 {{ end }}  )
   pkgs+=("${netbird_pkgs[@]}")
 {{ end }}
-{{ if .gui }}
+{{ if $gui }}
   local gui_pkgs=(
 {{ range .packages.gui.plasma }}{{ "  " }}{{ . | quote }}
 {{ end }}  )
   pkgs+=("${gui_pkgs[@]}")
 {{ end }}
-{{ if .workstation }}
+{{ if $workstation }}
   local ws_pkgs=(
 {{ range .packages.gui.workstation }}{{ "  " }}{{ . | quote }}
 {{ end }}  )
   pkgs+=("${ws_pkgs[@]}")
 {{ end }}
-{{ if eq .device_type "desktop" }}
+{{ if eq $device_type "desktop" }}
   local desktop_pkgs=(
 {{ range .packages.gui.desktop }}{{ "  " }}{{ . | quote }}
 {{ end }}  )
   pkgs+=("${desktop_pkgs[@]}")
 {{ end }}
-{{ if .has_nvidia }}
+{{ if $has_nvidia }}
   local nv_pkgs=(
 {{ range .packages.gui.nvidia }}{{ "  " }}{{ . | quote }}
 {{ end }}  )
   pkgs+=("${nv_pkgs[@]}")
 {{ end }}
-{{ if .has_battery }}
+{{ if $has_battery }}
   local lap_pkgs=(
 {{ range .packages.gui.laptop }}{{ "  " }}{{ . | quote }}
 {{ end }}  )
