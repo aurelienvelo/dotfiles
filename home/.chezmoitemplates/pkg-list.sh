82373e36d4998ee74fa696415367e08296b548d3
aurelienvelo <aurelien.velo@gmail.com>
2026-09-16 19:56:22 +0200

    add essentials & multi device & clean pacman

--- home/.chezmoitemplates/pkg-list.sh
+++ home/.chezmoitemplates/pkg-list.sh
@@ -4,11 +4,29 @@ desired_packages() {
 {{ range .packages.arch.pacman }}{{ "  " }}{{ . | quote }}
 {{ end }}{{ range .packages.arch.aur }}{{ "  " }}{{ . | quote }}
 {{ end }}  )
+{{ if ne .device_type "server" }}
+  local netbird_pkgs=(
+{{ range .packages.netbird.aur }}{{ "  " }}{{ . | quote }}
+{{ end }}  )
+  pkgs+=("${netbird_pkgs[@]}")
+{{ end }}
 {{ if .gui }}
   local gui_pkgs=(
 {{ range .packages.gui.plasma }}{{ "  " }}{{ . | quote }}
 {{ end }}  )
   pkgs+=("${gui_pkgs[@]}")
+{{ end }}
+{{ if .workstation }}
+  local ws_pkgs=(
+{{ range .packages.gui.workstation }}{{ "  " }}{{ . | quote }}
+{{ end }}  )
+  pkgs+=("${ws_pkgs[@]}")
+{{ end }}
+{{ if eq .device_type "desktop" }}
+  local desktop_pkgs=(
+{{ range .packages.gui.desktop }}{{ "  " }}{{ . | quote }}
+{{ end }}  )
+  pkgs+=("${desktop_pkgs[@]}")
 {{ end }}
 {{ if .has_nvidia }}
   local nv_pkgs=(
